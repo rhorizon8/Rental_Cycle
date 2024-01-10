8b4f3a291abcbfd44be7ee1a34b6f1a9c67b1ff3
aiueo-1234 <130837816+aiueo-1234@users.noreply.github.com>
2024-01-10 11:57:36 +0900

    sample

--- wordcloud-c#/WordCloud/Program.cs
+++ wordcloud-c#/WordCloud/Program.cs
@@ -1,1 +1,17 @@
-Console.WriteLine("aaa");
+using MeCab;
+
+var sentence = "行く川のながれは絶えずして、しかももとの水にあらず。";
+
+var parameter = new MeCabParam();
+var tagger = MeCabTagger.Create(parameter);
+
+foreach (var node in tagger.ParseToNodes(sentence))
+{
+    if (node.CharType > 0)
+    {
+        var features = node.Feature.Split(',');
+        var displayFeatures = string.Join(", ", features);
+
+        Console.WriteLine($"{node.Surface}\t{displayFeatures}");
+    }
+}
